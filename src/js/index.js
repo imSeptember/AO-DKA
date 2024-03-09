132554bbcb683f868dd58212135aabb768081000
Vladislav Kalenskiy <108691131+imSeptember@users.noreply.github.com>
2024-03-09 22:27:03 +0200

    1

--- src/js/index.js
+++ src/js/index.js
@@ -19,92 +19,117 @@ document.addEventListener('DOMContentLoaded', function () {
     });
 
     const form = document.getElementById('form');
+
     const submitButton = document.querySelector('.submit__button');
     form.addEventListener('submit', formSend);
 
-    async function formSend(e) {
-        e.preventDefault();
-
-        let error = formValidate(form);
-
-        let formData = new FormData(form);
-
-        if (error === 0) {
-            submitButton.classList.add('_sending');
-
-            let response = await fetch('sendmail.php', {
-                method: 'POST',
-                body: formData,
-            });
-            if (response.ok) {
-                let result = await response.json();
-                alert(result.message);
-                form.reset();
-                // form.classList.remove('._sending');
-            } else {
-                alert('Ошибка');
-                console.error();
-                form.classList.remove('._sending');
-            }
-        } else {
-            alert('Заполните обязательные поля');
-        }
+    function formSend() {
+        const userName = document.getElementById('username');
+        const phone = document.getElementById('telephone');
+        const email = document.getElementById('email');
+        let messageBody =
+            'Name ' +
+            userName +
+            '<br/> Phone ' +
+            phone +
+            '<br/> Email ' +
+            email;
+        Email.send({
+            Host: 'smtp.elasticemail.com',
+            Username: 'kalenskiyvlad@gmail.com',
+            Password: '76667601ECF56C5A07E797ABCE54D2D2637C',
+            To: 'kalenskiyvlad@gmail.com',
+            From: 'aodka@aodka.pp.ua',
+            Subject: 'Перезвонить клиенту',
+            Body: messageBody,
+        }).then((message) => alert(message));
+        submitButton.classList.add('_sending');
+        form.reset();
     }
 
-    function formValidate(form) {
-        let error = 0;
-        let formReq = document.querySelectorAll('._req');
+    // async function formSend(e) {
+    //     e.preventDefault();
 
-        for (let index = 0; index < formReq.length; index++) {
-            const input = formReq[index];
-            formRemoveError(input);
+    //     // let error = formValidate(form);
 
-            if (input.classList.contains('_email')) {
-                if (emailTest(input)) {
-                    formAddError(input);
-                    error++;
-                }
-            }
+    //     let formData = new FormData(form);
 
-            if (input.classList.contains('_phone')) {
-                if (phoneTest(input)) {
-                    formAddError(input);
-                    error++;
-                }
-            }
+    //     // if (error === 0) {
+    //     submitButton.classList.add('_sending');
 
-            if (input.classList.contains('_name')) {
-                if (nameTest(input)) {
-                    formAddError(input);
-                    error++;
-                }
-            }
-        }
+    //     let response = await fetch('sendmail.php', {
+    //         method: 'POST',
+    //         body: formData,
+    //     });
+    //     if (response.ok) {
+    //         let result = await response.json();
+    //         alert(result.message);
+    //         form.reset();
+    //         // form.classList.remove('._sending');
+    //     } else {
+    //         alert('Ошибка');
+    //         console.error();
+    //         form.classList.remove('._sending');
+    //     }
+    //     // } else {
+    //     //     alert('Заполните обязательные поля');
+    //     // }
+    // }
 
-        return error;
-    }
+    // function formValidate(form) {
+    //     let error = 0;
+    //     let formReq = document.querySelectorAll('._req');
 
-    function formAddError(input) {
-        input.parentElement.classList.add('_error');
-        input.classList.add('_error');
-    }
+    //     for (let index = 0; index < formReq.length; index++) {
+    //         const input = formReq[index];
+    //         formRemoveError(input);
 
-    function formRemoveError(input) {
-        input.parentElement.classList.remove('_error');
-        input.classList.remove('_error');
-    }
+    //         if (input.classList.contains('_email')) {
+    //             if (emailTest(input)) {
+    //                 formAddError(input);
+    //                 error++;
+    //             }
+    //         }
 
-    function emailTest(input) {
-        return !/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(input.value);
-    }
+    //         if (input.classList.contains('_phone')) {
+    //             if (phoneTest(input)) {
+    //                 formAddError(input);
+    //                 error++;
+    //             }
+    //         }
 
-    function phoneTest(input) {
-        return !/^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/.test(
-            input.value
-        );
-    }
+    //         if (input.classList.contains('_name')) {
+    //             if (nameTest(input)) {
+    //                 formAddError(input);
+    //                 error++;
+    //             }
+    //         }
+    //     }
 
-    function nameTest(input) {
-        return !/([a-zA-Z0-9_\s]+)/.test(input.value);
-    }
+    //     return error;
+    // }
+
+    // function formAddError(input) {
+    //     input.parentElement.classList.add('_error');
+    //     input.classList.add('_error');
+    // }
+
+    // function formRemoveError(input) {
+    //     input.parentElement.classList.remove('_error');
+    //     input.classList.remove('_error');
+    // }
+
+    // function emailTest(input) {
+    //     return !/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(input.value);
+    // }
+
+    // function phoneTest(input) {
+    //     return !/^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/.test(
+    //         input.value
+    //     );
+    // }
+
+    // function nameTest(input) {
+    //     return !/([a-zA-Z0-9_\s]+)/.test(input.value);
+    // }
 });
