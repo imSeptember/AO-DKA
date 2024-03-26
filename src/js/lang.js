c9f0cc38696120a59f35b2649dfee7582fc2b5c6
Vladislav Kalenskiy <108691131+imSeptember@users.noreply.github.com>
2024-03-26 22:35:26 +0200

    refactor + eng

---
+++ src/js/lang.js
@@ -0,0 +1,24 @@
+const select = document.querySelector('select');
+
+function loadContent(lang) {
+    if (lang === 'ua') {
+        location.href = '/ua/index.html';
+    } else {
+        location.href = '/en/index.html';
+    }
+}
+
+document.querySelector('select').addEventListener('change', function () {
+    let selectedLang = select.value;
+    if (selectedLang !== document.documentElement.lang) {
+        loadContent(selectedLang);
+    }
+});
+
+document
+    .querySelector(`option[value='${document.documentElement.lang}']`)
+    .setAttribute('selected', true);
+
+// DATE
+
+document.getElementById('currentYear').textContent = new Date().getFullYear();
